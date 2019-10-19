638829d28b7c73254a2f67868dd233efdfbf7225
Mia Pan <miapanyuewei@gmail.com>
2019-10-19 02:24:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,8 +35,18 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  
-  
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array, interger)
+  array.delete(interger)
+end
+
+
   
   
   
