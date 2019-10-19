5702c5867b3e023bd092d9d498162b597c285853
Mia Pan <miapanyuewei@gmail.com>
2019-10-19 02:19:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,22 @@ def using_concat(array1,array2)
 end
 
 def using_insert(array,element)
-  array.
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
